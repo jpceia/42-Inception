6cf94c8978f4b528d74181bbcbd62e568d3083fd
Joao Ceia <joao.p.ceia@gmail.com>
2022-02-16 05:17:43 +0000

    using mysqld_safe to keep session in the foreground

--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -17,4 +17,6 @@ echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 # use auth_socker plugin for authentication, it will check the user is connectig using a UNIX socker and then compares the username
 echo "update mysql.user set plugin='' where user='root';" | mysql -u root --skip-password
 
-bash
+service mysql stop
+
+mysqld_safe
