929a13c540a563c1076829ef46bfd19d59ec21df
Joao Ceia <joao.p.ceia@gmail.com>
2022-02-20 09:25:20 +0000

    updated mariadb scripts

--- srcs/requirements/mariadb/scripts/init.sh
+++ srcs/requirements/mariadb/scripts/init.sh
@@ -4,9 +4,10 @@ then
     echo "Wordpress database already exists"
 else
     echo "Setting wordpress database..."
-    envsubst < query.sql | mysqld --user=root --skip-password
+    envsubst < query.sql | mysqld --user=mysql --skip-password
     echo "Setting root pwd..."
-    envsubst < set_root_pwd.sql | mysqld --user=root --skip-password
+    # https://dev.mysql.com/doc/refman/8.0/en/resetting-permissions.html
+    envsubst < set_root_pwd.sql | mysqld --user=mysql --skip-password
     echo "Setup finished"
 fi
 
--- srcs/requirements/mariadb/scripts/set_root_pwd.sql
+++ srcs/requirements/mariadb/scripts/set_root_pwd.sql
@@ -1,5 +1,6 @@
-/* update root password */
-UPDATE mysql.user SET password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE user='root';
+/* update root password 
+UPDATE mysql.user SET password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE user='root';*/
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
 /* Reload the grant tables */
 FLUSH PRIVILEGES;
