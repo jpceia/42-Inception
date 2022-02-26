b39582da447f7f152987010992c06a420f9124ba
world <hello@world.com>
2022-02-26 15:20:54 +0000

    changed scripts to init mariadb

--- srcs/requirements/mariadb/scripts/init.sh
+++ srcs/requirements/mariadb/scripts/init.sh
@@ -3,12 +3,32 @@ if [ -d "/var/lib/mysql/wordpress" ];
 then
     echo "Wordpress database already exists"
 else
+    service mysql start
+
     echo "Setting wordpress database..."
-    envsubst < query.sql | mysqld --user=mysql --skip-password
-    echo "Setting root pwd..."
-    # https://dev.mysql.com/doc/refman/8.0/en/resetting-permissions.html
-    envsubst < set_root_pwd.sql | mysqld --user=mysql --skip-password
+
+    # Create Database
+    mysql -e "CREATE DATABASE wordpress;"
+    
+    # Create Admin USER
+    mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" # try with wordpress host
+    mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
+
+    # https://stackoverflow.com/questions/24270733/automate-mysql-secure-installation-with-echo-command-via-a-shell-script
+    # Make sure that NOBODY can access the server without a password
+    mysql -e "UPDATE mysql.user SET Password = PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User = 'root'"
+    mysql -e "DELETE FROM mysql.user WHERE User='';"
+    mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    mysql -e "DROP DATABASE IF EXISTS test;"
+    mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+    
+    # Make our changes take effect
+    mysql -e "FLUSH PRIVILEGES"
+
+    # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
     echo "Setup finished"
+
+    service mysql stop
 fi
 
-mysqld_safe
+exec "$@"
--- srcs/requirements/mariadb/scripts/set_root_pwd.sql
+++
@@ -1,6 +0,0 @@
-/* update root password 
-UPDATE mysql.user SET password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE user='root';*/
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-
-/* Reload the grant tables */
-FLUSH PRIVILEGES;
